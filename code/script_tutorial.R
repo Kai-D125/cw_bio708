b52e1a78df5f4d2cc0cfffecb67582c8882b56a1
Kai-D125 <kbdeleon@uncg.edu>
2025-08-26 10:00:39 -0400

    edit: script tutorial

--- code/script_tutorial.R
+++ code/script_tutorial.R
@@ -1,3 +1,7 @@
+
+# example code ------------------------------------------------------------
+
+
 x <- c(1, 2)
 x
 
@@ -12,3 +16,14 @@ mean(x)
 
 ## estimate SD
 sd(x)
+
+# excersize ---------------------------------------------------------------
+
+#Create a vector with length 10 assign it to z
+z<-1:10
+
+#Create a numeric matrix with 2 rows 2 columns, assign it to m
+m<-matrix(data=1:4, nrow = 2, ncol = 2)
+
+#data frame
+df0<-data.frame(name = c("Smith", "John", "Kate", "Akira"), height = c(154, 170, 156, 175))
